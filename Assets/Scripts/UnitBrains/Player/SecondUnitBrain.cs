5da04990390e746d6f2e54b10fb58854601bf302
Kvazarakht <beduncle@mail.ru>
2026-03-23 13:11:21 +0300

    Homework 1.3 (1st block, 3rd module)/Kvazarakht

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -19,9 +19,21 @@ namespace UnitBrains.Player
             ///////////////////////////////////////
             // Homework 1.3 (1st block, 3rd module)
             ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
+            if (GetTemperature() >= overheatTemperature)
+            {
+                return;
+            }
+
+            float i = 0f;
+
+            while (i <= _temperature)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+                i++;
+            }
             ///////////////////////////////////////
+            IncreaseTemperature();
         }
 
         public override Vector2Int GetNextStep()
